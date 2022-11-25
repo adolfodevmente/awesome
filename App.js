e8b7af1a949d6d497d5c61966da0b823cc56e188
adolfodevmente <117749550+adolfodevmente@users.noreply.github.com>
2022-11-25 10:58:32 -0500

    Update App.js

--- App.js
+++ App.js
@@ -76,7 +76,7 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            (Adolfo Julcamoro) Edit <Text style={styles.highlight}>App.js</Text> to change this
+            Editss <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
