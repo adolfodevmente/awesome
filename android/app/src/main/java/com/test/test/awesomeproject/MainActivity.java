6d19644f00b4a1625a379176801f824c26eefb4f
adolfodevmente <117749550+adolfodevmente@users.noreply.github.com>
2022-11-27 23:42:50 -0500

    fix

--- android/app/src/main/java/com/test/test/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/test/test/awesomeproject/MainActivity.java
@@ -46,4 +46,14 @@ public class MainActivity extends ReactActivity {
       return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     }
   }
+
+  public static final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final String APPLICATION_ID = "com.test.test.awesomeproject";
+    public static final String BUILD_TYPE = "debug";
+    public static final int VERSION_CODE = 1;
+    public static final String VERSION_NAME = "1.0";
+    // Field from default config.
+    public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
+  }
 }
