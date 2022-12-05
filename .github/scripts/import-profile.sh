1dcc68f541163cedccfdf8d50203d4bf9495c436
Adolfo Julcamoro <adolfo@devmente.com>
2022-12-05 16:36:23 -0500

    Change provisioning profile file name

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -3,8 +3,9 @@
 set -euo pipefail
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
-UUID=´/usr/libexec/PlistBuddy -c 'Print :UUID' /dev/stdin <<< $(security cms -D -i -./$FILE)´
-mv profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles\$UUID.mobileprovision
+FILE=profile.mobileprovision
+echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > $FILE
+UUID=`/usr/libexec/PlistBuddy -c 'Print :UUID' /dev/stdin <<< $(security cms -D -i ./$FILE)`
+mv "$FILE" "$HOME/Library/MobileDevice/Provisioning\ Profiles\$UUID.mobileprovision"
 #mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 #echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/$UUID.mobileprovision
