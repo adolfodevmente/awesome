b63b25e069bcc433a412e50311b9e19536b2ce10
Adolfo Julcamoro <adolfo@devmente.com>
2022-12-05 16:13:07 -0500

    Fix bug: first create folder

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
 UUID=´/usr/libexec/PlistBuddy -c 'Print :UUID' /dev/stdin <<< $(security cms -D -i -./$FILE)´
 mv profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles\$UUID.mobileprovision
 #mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
