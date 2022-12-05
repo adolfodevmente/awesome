1c142d9caab658597b71d3cf65424f6465d75f01
Adolfo Julcamoro <adolfo@devmente.com>
2022-12-05 14:31:50 -0500

    Change project name

--- ios/AwesomeProjectTests/AwesomeProjectTests.m
+++ ios/AwesomeProjectTests/AwesomeProjectTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface awesomeTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation awesomeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
