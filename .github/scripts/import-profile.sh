2398857cdd4a811972bcad7125898f631e8eba05
Adolfo Julcamoro <adolfo@devmente.com>
2022-12-06 12:08:21 -0500

    test

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -6,6 +6,6 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 FILE=profile.mobileprovision
 echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > $FILE
 UUID=`/usr/libexec/PlistBuddy -c 'Print :UUID' /dev/stdin <<< $(security cms -D -i ./$FILE)`
-mv "$FILE" "$HOME/Library/MobileDevice/Provisioning\ Profiles\$UUID.mobileprovision"
+mv "$FILE" "~/Library/MobileDevice/Provisioning\ Profiles\$UUID.mobileprovision"
 #mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 #echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/$UUID.mobileprovision
